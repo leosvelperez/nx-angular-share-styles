ef395c34ad7e022de30d042adb38b0d05b3ead52
Leosvel Pérez Espinosa <leosvel.perez.espinosa@gmail.com>
2024-02-29 17:33:32 +0100

    set up app + shared components and styles

--- apps/app1/src/app/app.component.ts
+++ apps/app1/src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { UiComponent } from '@my-org/ui';
+import { UiBuildableComponent } from '@my-org/ui-buildable';
+import { UiPublishableComponent } from '@my-org/ui-publishable';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    UiComponent,
+    UiBuildableComponent,
+    UiPublishableComponent,
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
